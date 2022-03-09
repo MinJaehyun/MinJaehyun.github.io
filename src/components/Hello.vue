5c4be5b60973053f53b05745adbf7534804baa29
{krism01} <{krism01@naver.com}>
2022-03-09 10:31:32 +0900

    파일명 변경 및 setTimeout 설정

---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h4>방문 해주셔서 감사합니다 :)</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  mounted() {
+    setTimeout(() => {
+      this.$store.commit("setVisit", false);
+    }, 6000);
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/helloall.css";
+</style>
