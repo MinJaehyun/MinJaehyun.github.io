e05c18f7c83fea4324d56d880a0f72794b47a130
{krism01} <{krism01@naver.com}>
2022-03-09 10:32:58 +0900

    중복 코드를 반복문 처리 및 내림차순 버튼 생성하여 최신순 정렬 기능 추가

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <h1 style="text-align: left;">
+      Personal Activities
+    </h1>
+    <button style="float: right;" class="btn btn-primary" @click="ascendingNew">
+      최신순 정렬
+    </button>
+    <div class="row">
+      <!-- v-for -->
+      <div
+        class="col-lg-4 mb-4"
+        ontouchstart="this.classList.toggle('hover');"
+        v-for="item in $store.state.post"
+        :key="item"
+      >
+        <div class="container">
+          <!-- front -->
+          <div
+            class="front"
+            :style="{
+              backgroundImage: `url(${item.front.url})`,
+            }"
+          >
+            <div class="inner">
+              <p>{{ item.front.title }}</p>
+              <span>{{ item.front.date }}</span>
+            </div>
+          </div>
+          <!-- back -->
+          <div class="back">
+            <div class="inner">
+              <p>{{ item.back.title }}</p>
+              <p>{{ item.back.content }}</p>
+              <br />
+              <a
+                :href="item.back.url"
+                target="_blank"
+                style="text-decoration : none; color: white"
+              >
+                배포 링크
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: "Portfolio",
+  methods: {
+    ascendingNew() {
+      this.$store.state.post.sort(function(a, b) {
+        return b.id - a.id;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/card.css";
+</style>
